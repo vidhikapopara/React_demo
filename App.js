8c1f1849a9a4256a01c1787da881d9cbe8ba88fc
vidhikapopara <109406644+vidhikapopara@users.noreply.github.com>
2023-08-31 18:55:17 +0530

    twitter user

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import "./App.css";
 import React from "react";
-import Home from "./components/Homee";
+import Twitter from "./admin/User";
+import Main from "./main/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      {/* <Nav /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Main />} />
+          <Route path="/admin/twitter" exact element={<Twitter />} />
+          {/* <Route
+            path="/admin/products/create"
+            exact
+            element={<ProductCreate />}
+          />
+          <Route
+            path="/admin/products/:id/edit"
+            exact
+            element={<ProductEdit />}
+          /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
